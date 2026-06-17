de36b32be85907b9e125b729f2fdd02ef830d8ce
wjh123-321 <2786248572@qq.com>
2026-06-17 14:04:57 +0800

    Add app/services/__init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,12 @@
+from .ai_service import AIService, LLMClient
+from .repair_recommender import RepairRecommender
+from .parts_forecaster import PartsForecaster
+from .customer_analyzer import CustomerAnalyzer
+
+__all__ = [
+    "AIService",
+    "LLMClient",
+    "RepairRecommender",
+    "PartsForecaster",
+    "CustomerAnalyzer",
+]
